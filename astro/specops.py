37f2b8c9d526f4ef1a0480f5e3d4574114932059
Seth <askewseth@gmail.com>
2016-03-16 20:34:10 -0400

    Changed some imports around to work better

--- astro/specops.py
+++ astro/specops.py
@@ -4,15 +4,15 @@ import sys
 import random as rand
 import locations as loc
 import myfun as m
+# import myfun.astro as ast
 sys.path.insert(0, '/home/' + loc.get_location()[1] + '/AstroML/')
 from Spectrum import spectrum
-
 
 def rspec(path=None, local=False, filt=""):
     """Return a random spectra built from fits file in given path."""
     # Get path
     if path is None:
-        path = m.shortpath('All')
+        path = loc.shortpath('All')
     if local:
         path = '/home/extra/AstroFiles/fits/'
     fits = [x for x in os.listdir(path) if '.fits' in x]
@@ -38,9 +38,9 @@ def rspecs(num=1, path=None, local=False, filt=''):
     """Allow for getting a list of multiple specs."""
     # Get path
     if path is None:
-        path = m.shortpath('All')
+        path = loc.shortpath('All')
     if local:
-        path = m.shortpath('local')
+        path = loc.shortpath('local')
     fits = [x for x in m.ls(True, path=path) if '.fits' in x]
     fnames = [x for x in fits if filt in x]
     assert len(fits) != 0, "There were no fit's files in the path"
@@ -67,7 +67,7 @@ def plot_rspecs(num=1, ret=False, path=None,
     """Plot the HA ord of <num> spectra."""
     # Get path
     if path is None:
-        path = m.shortpath('All')
+        path = loc.shortpath('All')
         print 'got new path'
     specs = rspecs(num, path, local)
     bad = []
@@ -88,10 +88,10 @@ def all_specs(filt='', path=None, local=False, close=True):
     """Return all of the files in the path given as spectra."""
     # Get path
     if path is None:
-        path = m.shortpath('All')
+        path = loc.shortpath('All')
         print 'got new path'
     if local:
-        path = m.shortpath('local')
+        path = loc.shortpath('local')
     specs = []
     m.cd(path)
     m.pwd()
@@ -115,7 +115,7 @@ def all_objs(count=False, path=None):
     """Return all spectra as well as the number of occs for each spectra."""
     # Get path
     if path is None:
-        path = m.shortpath('All')
+        path = loc.shortpath('All')
         print 'got new path'
     specs = {}
     specnum = {}
@@ -150,7 +150,7 @@ def all_objs_info(path=None):
     """Return all spectra as well as the number of occs for each spectra."""
     # Get path
     if path is None:
-        path = m.shortpath('All')
+        path = loc.shortpath('All')
         print 'got new path'
     specs = {}
     m.cd(path)
