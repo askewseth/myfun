72de4a438e554316a0b2044031a15189a53687c5
Seth <askewseth@gmail.com>
2016-03-24 10:48:10 -0400

    Trying to fix import errors with stats.py

--- astro/__init__.py
+++ astro/__init__.py
@@ -1,9 +1,11 @@
 """Initalizer for astro app in usefulfunctions module."""
 from locations import *
 from specops import *
+import sys
+import os
+sys.path.insert(0, os.getcwd())
 from stats import *
 from statstest import *
-import os
 
 
 loc, usr = get_location()
