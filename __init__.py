a4012df24fc05fc10ff3e38ca7f720ab792bfb6e
Seth <askewseth@gmail.com>
2016-03-24 10:50:29 -0400

    Attempting to fix import errors with stats.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 """Initalizer for my commonly used functions."""
 import os
-path = os.getcwd()
+path = os.getcwd() + '/'
 from usefun import *
 from printers import *
-from dirs import *
+# from dirs import *
 from colorfun import *
 from picklefun import *
 from locations2 import *
 from oneliners import *
+import sys
+sys.path.insert(0, path + 'astro/')
 import astro
 
 
